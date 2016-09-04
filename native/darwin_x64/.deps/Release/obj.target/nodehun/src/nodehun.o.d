52e602315b2faf7e76e2fed95b283e756fb9cf60
Thomas Beverley <tom.beverley101@gmail.com>
2016-09-04 14:28:34 +0100

    Updated for electron

--- native/darwin_x64/.deps/Release/obj.target/nodehun/src/nodehun.o.d
+++ native/darwin_x64/.deps/Release/obj.target/nodehun/src/nodehun.o.d
@@ -1,15 +1,15 @@
-cmd_Release/obj.target/nodehun/src/nodehun.o := g++-4.8 '-DNODE_GYP_MODULE_NAME=nodehun' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DHUNSPELL_STATIC' '-DBUILDING_NODE_EXTENSION' -I/Users/travis/.node-gyp/6.3.0/include/node -I/Users/travis/.node-gyp/6.3.0/src -I/Users/travis/.node-gyp/6.3.0/deps/uv/include -I/Users/travis/.node-gyp/6.3.0/deps/v8/include -I../src/hunspell/src/hunspell  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/nodehun/src/nodehun.o.d.raw   -c -o Release/obj.target/nodehun/src/nodehun.o ../src/nodehun.cpp
+cmd_Release/obj.target/nodehun/src/nodehun.o := g++-4.8 '-DNODE_GYP_MODULE_NAME=nodehun' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DHUNSPELL_STATIC' '-DBUILDING_NODE_EXTENSION' -I/Users/travis/.node-gyp/iojs-1.3.4/include/node -I/Users/travis/.node-gyp/iojs-1.3.4/src -I/Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include -I/Users/travis/.node-gyp/iojs-1.3.4/deps/v8/include -I../src/hunspell/src/hunspell  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/nodehun/src/nodehun.o.d.raw   -c -o Release/obj.target/nodehun/src/nodehun.o ../src/nodehun.cpp
 Release/obj.target/nodehun/src/nodehun.o: ../src/nodehun.cpp \
  ../src/license.nodehun \
- /Users/travis/.node-gyp/6.3.0/include/node/node_version.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/src/node_version.h \
  ../src/post0.12.0/nodehun.cpp ../src/post0.12.0/nodehun.hpp \
- /Users/travis/.node-gyp/6.3.0/include/node/uv.h \
- /Users/travis/.node-gyp/6.3.0/include/node/uv-errno.h \
- /Users/travis/.node-gyp/6.3.0/include/node/uv-version.h \
- /Users/travis/.node-gyp/6.3.0/include/node/uv-unix.h \
- /Users/travis/.node-gyp/6.3.0/include/node/uv-threadpool.h \
- /Users/travis/.node-gyp/6.3.0/include/node/uv-darwin.h \
- /Users/travis/.node-gyp/6.3.0/include/node/pthread-barrier.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-errno.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-version.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-unix.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-threadpool.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-darwin.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/pthread-barrier.h \
  ../src/hunspell/src/hunspell/hunspell.hxx \
  ../src/hunspell/src/hunspell/hunvisapi.h \
  ../src/hunspell/src/hunspell/hashmgr.hxx \
@@ -25,26 +25,26 @@ Release/obj.target/nodehun/src/nodehun.o: ../src/nodehun.cpp \
  ../src/hunspell/src/hunspell/replist.hxx \
  ../src/hunspell/src/hunspell/suggestmgr.hxx \
  ../src/hunspell/src/hunspell/langnum.hxx \
- /Users/travis/.node-gyp/6.3.0/include/node/node.h \
- /Users/travis/.node-gyp/6.3.0/include/node/v8.h \
- /Users/travis/.node-gyp/6.3.0/include/node/v8-version.h \
- /Users/travis/.node-gyp/6.3.0/include/node/v8config.h \
- /Users/travis/.node-gyp/6.3.0/include/node/node_version.h \
- /Users/travis/.node-gyp/6.3.0/include/node/node_buffer.h \
- /Users/travis/.node-gyp/6.3.0/include/node/node.h \
- /Users/travis/.node-gyp/6.3.0/include/node/node_object_wrap.h
+ /Users/travis/.node-gyp/iojs-1.3.4/src/node.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8-version.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8config.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/src/node_version.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/src/node_buffer.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/src/node.h \
+ /Users/travis/.node-gyp/iojs-1.3.4/src/node_object_wrap.h
 ../src/nodehun.cpp:
 ../src/license.nodehun:
-/Users/travis/.node-gyp/6.3.0/include/node/node_version.h:
+/Users/travis/.node-gyp/iojs-1.3.4/src/node_version.h:
 ../src/post0.12.0/nodehun.cpp:
 ../src/post0.12.0/nodehun.hpp:
-/Users/travis/.node-gyp/6.3.0/include/node/uv.h:
-/Users/travis/.node-gyp/6.3.0/include/node/uv-errno.h:
-/Users/travis/.node-gyp/6.3.0/include/node/uv-version.h:
-/Users/travis/.node-gyp/6.3.0/include/node/uv-unix.h:
-/Users/travis/.node-gyp/6.3.0/include/node/uv-threadpool.h:
-/Users/travis/.node-gyp/6.3.0/include/node/uv-darwin.h:
-/Users/travis/.node-gyp/6.3.0/include/node/pthread-barrier.h:
+/Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv.h:
+/Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-errno.h:
+/Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-version.h:
+/Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-unix.h:
+/Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-threadpool.h:
+/Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-darwin.h:
+/Users/travis/.node-gyp/iojs-1.3.4/deps/uv/include/pthread-barrier.h:
 ../src/hunspell/src/hunspell/hunspell.hxx:
 ../src/hunspell/src/hunspell/hunvisapi.h:
 ../src/hunspell/src/hunspell/hashmgr.hxx:
@@ -60,11 +60,11 @@ Release/obj.target/nodehun/src/nodehun.o: ../src/nodehun.cpp \
 ../src/hunspell/src/hunspell/replist.hxx:
 ../src/hunspell/src/hunspell/suggestmgr.hxx:
 ../src/hunspell/src/hunspell/langnum.hxx:
-/Users/travis/.node-gyp/6.3.0/include/node/node.h:
-/Users/travis/.node-gyp/6.3.0/include/node/v8.h:
-/Users/travis/.node-gyp/6.3.0/include/node/v8-version.h:
-/Users/travis/.node-gyp/6.3.0/include/node/v8config.h:
-/Users/travis/.node-gyp/6.3.0/include/node/node_version.h:
-/Users/travis/.node-gyp/6.3.0/include/node/node_buffer.h:
-/Users/travis/.node-gyp/6.3.0/include/node/node.h:
-/Users/travis/.node-gyp/6.3.0/include/node/node_object_wrap.h:
+/Users/travis/.node-gyp/iojs-1.3.4/src/node.h:
+/Users/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8.h:
+/Users/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8-version.h:
+/Users/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8config.h:
+/Users/travis/.node-gyp/iojs-1.3.4/src/node_version.h:
+/Users/travis/.node-gyp/iojs-1.3.4/src/node_buffer.h:
+/Users/travis/.node-gyp/iojs-1.3.4/src/node.h:
+/Users/travis/.node-gyp/iojs-1.3.4/src/node_object_wrap.h:
--- native/linux_x64/.deps/Release/obj.target/nodehun/src/nodehun.o.d
+++ native/linux_x64/.deps/Release/obj.target/nodehun/src/nodehun.o.d
@@ -1,14 +1,14 @@
-cmd_Release/obj.target/nodehun/src/nodehun.o := g++-4.8 '-DNODE_GYP_MODULE_NAME=nodehun' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DHUNSPELL_STATIC' '-DBUILDING_NODE_EXTENSION' -I/home/travis/.node-gyp/6.3.0/include/node -I/home/travis/.node-gyp/6.3.0/src -I/home/travis/.node-gyp/6.3.0/deps/uv/include -I/home/travis/.node-gyp/6.3.0/deps/v8/include -I../src/hunspell/src/hunspell  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/nodehun/src/nodehun.o.d.raw   -c -o Release/obj.target/nodehun/src/nodehun.o ../src/nodehun.cpp
+cmd_Release/obj.target/nodehun/src/nodehun.o := g++-4.8 '-DNODE_GYP_MODULE_NAME=nodehun' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DHUNSPELL_STATIC' '-DBUILDING_NODE_EXTENSION' -I/home/travis/.node-gyp/iojs-1.3.4/include/node -I/home/travis/.node-gyp/iojs-1.3.4/src -I/home/travis/.node-gyp/iojs-1.3.4/deps/uv/include -I/home/travis/.node-gyp/iojs-1.3.4/deps/v8/include -I../src/hunspell/src/hunspell  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/nodehun/src/nodehun.o.d.raw   -c -o Release/obj.target/nodehun/src/nodehun.o ../src/nodehun.cpp
 Release/obj.target/nodehun/src/nodehun.o: ../src/nodehun.cpp \
  ../src/license.nodehun \
- /home/travis/.node-gyp/6.3.0/include/node/node_version.h \
+ /home/travis/.node-gyp/iojs-1.3.4/src/node_version.h \
  ../src/post0.12.0/nodehun.cpp ../src/post0.12.0/nodehun.hpp \
- /home/travis/.node-gyp/6.3.0/include/node/uv.h \
- /home/travis/.node-gyp/6.3.0/include/node/uv-errno.h \
- /home/travis/.node-gyp/6.3.0/include/node/uv-version.h \
- /home/travis/.node-gyp/6.3.0/include/node/uv-unix.h \
- /home/travis/.node-gyp/6.3.0/include/node/uv-threadpool.h \
- /home/travis/.node-gyp/6.3.0/include/node/uv-linux.h \
+ /home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv.h \
+ /home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-errno.h \
+ /home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-version.h \
+ /home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-unix.h \
+ /home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-threadpool.h \
+ /home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-linux.h \
  ../src/hunspell/src/hunspell/hunspell.hxx \
  ../src/hunspell/src/hunspell/hunvisapi.h \
  ../src/hunspell/src/hunspell/hashmgr.hxx \
@@ -24,25 +24,25 @@ Release/obj.target/nodehun/src/nodehun.o: ../src/nodehun.cpp \
  ../src/hunspell/src/hunspell/replist.hxx \
  ../src/hunspell/src/hunspell/suggestmgr.hxx \
  ../src/hunspell/src/hunspell/langnum.hxx \
- /home/travis/.node-gyp/6.3.0/include/node/node.h \
- /home/travis/.node-gyp/6.3.0/include/node/v8.h \
- /home/travis/.node-gyp/6.3.0/include/node/v8-version.h \
- /home/travis/.node-gyp/6.3.0/include/node/v8config.h \
- /home/travis/.node-gyp/6.3.0/include/node/node_version.h \
- /home/travis/.node-gyp/6.3.0/include/node/node_buffer.h \
- /home/travis/.node-gyp/6.3.0/include/node/node.h \
- /home/travis/.node-gyp/6.3.0/include/node/node_object_wrap.h
+ /home/travis/.node-gyp/iojs-1.3.4/src/node.h \
+ /home/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8.h \
+ /home/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8-version.h \
+ /home/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8config.h \
+ /home/travis/.node-gyp/iojs-1.3.4/src/node_version.h \
+ /home/travis/.node-gyp/iojs-1.3.4/src/node_buffer.h \
+ /home/travis/.node-gyp/iojs-1.3.4/src/node.h \
+ /home/travis/.node-gyp/iojs-1.3.4/src/node_object_wrap.h
 ../src/nodehun.cpp:
 ../src/license.nodehun:
-/home/travis/.node-gyp/6.3.0/include/node/node_version.h:
+/home/travis/.node-gyp/iojs-1.3.4/src/node_version.h:
 ../src/post0.12.0/nodehun.cpp:
 ../src/post0.12.0/nodehun.hpp:
-/home/travis/.node-gyp/6.3.0/include/node/uv.h:
-/home/travis/.node-gyp/6.3.0/include/node/uv-errno.h:
-/home/travis/.node-gyp/6.3.0/include/node/uv-version.h:
-/home/travis/.node-gyp/6.3.0/include/node/uv-unix.h:
-/home/travis/.node-gyp/6.3.0/include/node/uv-threadpool.h:
-/home/travis/.node-gyp/6.3.0/include/node/uv-linux.h:
+/home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv.h:
+/home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-errno.h:
+/home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-version.h:
+/home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-unix.h:
+/home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-threadpool.h:
+/home/travis/.node-gyp/iojs-1.3.4/deps/uv/include/uv-linux.h:
 ../src/hunspell/src/hunspell/hunspell.hxx:
 ../src/hunspell/src/hunspell/hunvisapi.h:
 ../src/hunspell/src/hunspell/hashmgr.hxx:
@@ -58,11 +58,11 @@ Release/obj.target/nodehun/src/nodehun.o: ../src/nodehun.cpp \
 ../src/hunspell/src/hunspell/replist.hxx:
 ../src/hunspell/src/hunspell/suggestmgr.hxx:
 ../src/hunspell/src/hunspell/langnum.hxx:
-/home/travis/.node-gyp/6.3.0/include/node/node.h:
-/home/travis/.node-gyp/6.3.0/include/node/v8.h:
-/home/travis/.node-gyp/6.3.0/include/node/v8-version.h:
-/home/travis/.node-gyp/6.3.0/include/node/v8config.h:
-/home/travis/.node-gyp/6.3.0/include/node/node_version.h:
-/home/travis/.node-gyp/6.3.0/include/node/node_buffer.h:
-/home/travis/.node-gyp/6.3.0/include/node/node.h:
-/home/travis/.node-gyp/6.3.0/include/node/node_object_wrap.h:
+/home/travis/.node-gyp/iojs-1.3.4/src/node.h:
+/home/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8.h:
+/home/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8-version.h:
+/home/travis/.node-gyp/iojs-1.3.4/deps/v8/include/v8config.h:
+/home/travis/.node-gyp/iojs-1.3.4/src/node_version.h:
+/home/travis/.node-gyp/iojs-1.3.4/src/node_buffer.h:
+/home/travis/.node-gyp/iojs-1.3.4/src/node.h:
+/home/travis/.node-gyp/iojs-1.3.4/src/node_object_wrap.h:
